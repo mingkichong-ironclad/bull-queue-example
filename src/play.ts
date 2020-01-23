ad81fdda29bc5886d3a182fc483e769f3571bb7d
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-01-23 14:45:28 -0800

    add bull-board, queue background jobs inspector

--- src/play.ts
+++ src/play.ts
@@ -1,10 +1,15 @@
 import * as Queue from 'bull';
+import * as Bull_Board from 'bull-board';
+import * as Express from 'express';
 
 const QUEUE_NAME = 'Bull Queue Example';
 const REDIS_URL = 'redis://127.0.0.1:6379';
+const LOCAL_LISTENING_PORT = 56789;
 
 const IS_ERROR = true;
 const TIMEOUT_DURATION = 2000;
+
+const app = Express();
 
 const queue = new Queue(QUEUE_NAME, {
   redis: {
@@ -13,16 +18,20 @@ const queue = new Queue(QUEUE_NAME, {
   }
 });
 
-let promise;
+// bull-board setup
+Bull_Board.setQueues([queue]);
+app.use('/admin/queues', Bull_Board.UI);
+
+app.listen(LOCAL_LISTENING_PORT, () => console.log("Server started on port " + LOCAL_LISTENING_PORT)); // http://localhost:56789/
 
 console.log("===   Submit JOB   ===");
 console.log(new Date().toLocaleString());
-promise = queue.add({ job_key: "job_value" });
+let promise = queue.add({ job_key: "job_value" });
 console.log("======================\n");
 
 queue.process((job) => {
   return new Promise<Queue.Job>(function(resolve, reject) {
-  console.log("===   PROCESS JOB   ===");
+    console.log("===   PROCESS JOB   ===");
     console.log(new Date().toLocaleString());
     console.log("============================\n");
     console.log(job.data);

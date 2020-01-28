93e3b359a97d7a39883eecf713f74664933c62a5
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-01-28 10:30:22 -0800

    move num queues definition to file 'conf/NUM_QUEUES'

--- send_jobs.sh
+++ send_jobs.sh
@@ -3,7 +3,7 @@
 # send a job to a randomly picked queue, and repeat $1 times
 
 let repeats=1
-let NUM_OF_QUEUES=50
+typeset -i NUM_OF_QUEUES=$(cat 'conf/NUM_QUEUES')
 
 if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -gt "1" ]; then
   repeats=$1
--- src/queue.ts
+++ src/queue.ts
@@ -1,6 +1,13 @@
 import * as BullQueue from 'bull';
+import * as FS from 'fs';
 
-export const NUM_OF_QUEUES = 50;
+const numQueueFileName = 'conf/NUM_QUEUES';
+const NUM_QUEUES_STRING_FROM_FILE = FS.readFileSync(numQueueFileName).toString('utf8');
+export const NUM_OF_QUEUES = parseInt(NUM_QUEUES_STRING_FROM_FILE);
+if (!NUM_OF_QUEUES) {
+  console.error(`Invalid number of queues "${NUM_QUEUES_STRING_FROM_FILE}" from file "${numQueueFileName}"`);
+  process.exit(1);
+}
 export const QUEUE_NAME_PREFIX = 'Bull Queue Example';
 export const REDIS_HOST = "127.0.0.1";
 export const REDIS_PORT = 6379;
@@ -8,8 +15,8 @@ const REDIS_URL = `redis://${REDIS_HOST}:${REDIS_PORT}`;
 
 export const queues: Array<BullQueue.Queue> = [];
 
-for(let i = 0; i < NUM_OF_QUEUES; i++){
-  queues.push(new BullQueue(QUEUE_NAME_PREFIX + "--"+ (i+1), REDIS_URL));
+for (let i = 0; i < NUM_OF_QUEUES; i++) {
+  queues.push(new BullQueue(QUEUE_NAME_PREFIX + "--" + (i + 1), REDIS_URL));
 }
 
 export async function sendData(bullQueue: BullQueue.Queue, data: MockData) {

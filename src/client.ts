0cd2387d3cf092838b02aaa12c04dc413e6c8388
Ming_Ki Chong <mingki.chong@ironcladapp.com>
2020-01-28 13:05:32 -0800

    change client allow user to send a job to a specific queue

--- src/client.ts
+++ src/client.ts
@@ -2,9 +2,16 @@ import { queues, sendData, getMockData } from './queue';
 import * as Path from 'path';
 
 const thisFileName = Path.basename(__filename);
+let queueIndex = parseInt(process.argv[2]) - 1;
+if(isNaN(queueIndex) || queueIndex < 0 || queueIndex > queues.length-1){
+  queueIndex = Math.floor(Math.random() * queues.length);
+}
 
-const queue = queues[0]; // only send to the first queue
-sendData(queue, getMockData(thisFileName)).then((returnString)=>{
+const queue = queues[queueIndex];
+const promise = sendData(queue, getMockData(thisFileName)).then((returnString) => {
   console.log(returnString);
   queue.close();
 });
+
+const proc = process;
+promise.finally(() => proc.exit(0));
